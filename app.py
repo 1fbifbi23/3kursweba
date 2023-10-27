91b4ca40639dd17df8e929a9394254201b83e83e
1fbifbi23 <danii.llarionov111@gmail.com>
2023-10-27 23:55:31 +0700

    pycache

--- app.py
+++ app.py
@@ -3,4 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
-    return"web-server on Flask"
+    return """
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ,ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+        <a>Лабораторная 1</a>
+        <footer>
+            &copy; Даниил Ларионов, ФБИ-12, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
