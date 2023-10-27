9b9088bcec5b7b4cef5bf50c9cd6298c5c9432b1
1fbifbi23 <danii.llarionov111@gmail.com>
2023-10-28 01:03:40 +0700

    циклы + книги

--- app.py
+++ app.py
@@ -156,4 +156,24 @@ def example():
     course = '2 курс'
     group = 'ФБИ-14'
     number_laboratory = '2'
-    return render_template('example.html', name=name, course=course, group=group, number_laboratory=number_laboratory)
+    fruits = [
+        {'name': 'Яблоки', 'price': 100},
+        {'name': 'Груши', 'price': 166},
+        {'name': 'Апельсины', 'price': 320},
+        {'name': 'Бананы', 'price': 1030},
+        {'name': 'Манго', 'price': 1520}
+        ]
+    
+    books = [
+        {'name': 'Куджо', 'author': 'Стивен Кинг', 'size': '400 страниц'},
+        {'name': 'Мастер и Маргарита', 'author': 'Михаил Булгаков', 'size': '350 страниц'},
+        {'name': '1984', 'author': 'Джордж Оруэлл', 'size': '328 страниц'},
+        {'name': 'Война и мир', 'author': 'Лев Толстой', 'size': '1225 страниц'},
+        {'name': 'Анна Каренина', 'author': 'Лев Толстой', 'size': '864 страницы'},
+        {'name': 'Гарри Поттер и философский камень', 'author': 'Джоан Роулинг', 'size': '223 страницы'},
+        {'name': 'Маленький принц', 'author': 'Антуан де Сент-Экзюпери', 'size': '96 страниц'},
+        {'name': 'Три товарища', 'author': 'Эрих Мария Ремарк', 'size': '480 страниц'},
+        {'name': 'Алхимик', 'author': 'Пауло Коэльо', 'size': '208 страниц'},
+        {'name': 'Метро 2033', 'author': 'Дмитрий Глуховский', 'size': '416 страниц'}
+        ]
+    return render_template('example.html', name=name, course=course, group=group, number_laboratory=number_laboratory, fruits=fruits, books=books)
