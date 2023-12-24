e7b778a569a4777a691c6d6b94316ef0345bd77d
1fbifbi23 <danii.llarionov111@gmail.com>
2023-12-24 20:02:03 +0700

    9 лаба

---
+++ lab9.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, redirect, url_for, render_template, request, session, abort
+from datetime import datetime
+
+lab9 = Blueprint('lab9', __name__)
+
+
+@lab9.route('/lab9/')
+def lab():
+    return render_template('lab9/index.html')
+
+@lab9.app_errorhandler(404)
+def not_found(e):
+    return render_template('lab9/404.html')
+
+@lab9.app_errorhandler(500)
+def internal_error(e):
+     return render_template('lab9/500.html')
+
+@lab9.route('/lab9/500')
+def error_500():
+    abort(500)
+
+@lab9.route('/lab9/greeting_card', methods=['GET', 'POST'])
+def greeting_card():
+    if request.method == 'POST':
+        recipient_name = request.form.get('recipient_name')
+        recipient_gender = request.form.get('recipient_gender')
+        sender_name = request.form.get('sender_name')
+        return render_template('lab9/greeting_card.html', recipient_name=recipient_name, recipient_gender=recipient_gender, sender_name=sender_name)
+    else:
+        recipient_name = request.args.get('recipient_name')
+        recipient_gender = request.args.get('recipient_gender')
+        sender_name = request.args.get('sender_name')
+        return render_template('lab9/greeting_card.html', recipient_name=recipient_name, recipient_gender=recipient_gender, sender_name=sender_name)
